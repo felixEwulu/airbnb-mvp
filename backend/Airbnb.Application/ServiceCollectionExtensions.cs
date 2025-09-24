d3225dbea3b9f2621cc7e1b88c8be9d122064298
temidayo titilola <Ttmvalid3110@gmail.com>
2025-09-23 22:29:31 -0700

    Update ServiceCollectionExtensions.cs

--- backend/Airbnb.Application/ServiceCollectionExtensions.cs
+++ backend/Airbnb.Application/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 using Airbnb.Application.Services;
 using Airbnb.Application.Interfaces;
 
@@ -11,9 +12,17 @@ namespace Airbnb.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceCollectionExtensions).Assembly));
-            services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
-            services.AddValidatorsFromAssembly(typeof(ServiceCollectionExtensions).Assembly);
+            // MediatR (v12+)
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            });
+
+            // AutoMapper
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            // FluentValidation
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
             // Register application services
             services.AddScoped<IUserService, UserService>();
